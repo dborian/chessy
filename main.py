a8ef094a5f25f8922fde5f7668d73c146fd060e7
dborian <edmedorian@gmail.com>
2026-01-21 19:06:04 +0100

    new texture

--- main.py
+++ main.py
@@ -19,8 +19,7 @@ class pion:
 
 class texture:
     def __init__(self):
-        self.whitepace = None
-        self.greypace = None
+        self.board = None
         self.greenpace = None
         self.orange = None
         self.redpace = None
@@ -37,81 +36,84 @@ class info:
         self.yp = 0
 
 def display_pace(data):
-    x = 0
-    y = 0
-    b = 0
-    while y != 9:
-        if b == 0:
-            data.fenetre.blit(data.text.whitepace,(x*130,y*130))
-            b = 1
-        else:
-            data.fenetre.blit(data.text.greypace,(x*130,y*130))
-            b = 0
-        x = x + 1
-        if x == 9:
-            x = 0
-            y = y + 1
+    data.fenetre.blit(data.text.board,(0, 0))    
 
 def has_upper(s):
     return any(c.isupper() for c in s)
 
 def display_piece(data):
-    data.fenetre.blit(data.text.greenpace,(data.xp*130, data.yp*130))
+    data.fenetre.blit(data.text.greenpace,(data.xp*128, data.yp*128))
     for row in range(8):
         for col in range(8):
             if data.board2[row][col] == "0" and row == data.yp and col == data.xp:
-                data.fenetre.blit(data.text.orangepace,(col*130,row*130))
+                data.fenetre.blit(data.text.orangepace,(col*128,row*128))
             elif data.board2[row][col] == "0":
-                data.fenetre.blit(data.text.redpace,(col*130,row*130))
+                data.fenetre.blit(data.text.redpace,(col*128,row*128))
             if data.board[row][col] == "re":
-                data.fenetre.blit(data.text.white.re,(col*130,row*130))
+                data.fenetre.blit(data.text.white.re,(col*128,row*128))
             elif data.board[row][col] == "ro":
-                data.fenetre.blit(data.text.white.ro,(col*130,row*130))
+                data.fenetre.blit(data.text.white.ro,(col*128,row*128))
             elif data.board[row][col] == "fo":
-                data.fenetre.blit(data.text.white.fo,(col*130,row*130))
+                data.fenetre.blit(data.text.white.fo,(col*128,row*128))
             elif data.board[row][col] == "ca":
-                data.fenetre.blit(data.text.white.ca,(col*130,row*130))
+                data.fenetre.blit(data.text.white.ca,(col*128,row*128))
             elif data.board[row][col] == "to":
-                data.fenetre.blit(data.text.white.to,(col*130,row*130))
+                data.fenetre.blit(data.text.white.to,(col*128,row*128))
             elif data.board[row][col] == "pi":
-                data.fenetre.blit(data.text.white.pi,(col*130,row*130))
+                data.fenetre.blit(data.text.white.pi,(col*128,row*128))
             elif data.board[row][col] == "RE":
-                data.fenetre.blit(data.text.black.re,(col*130,row*130))
+                data.fenetre.blit(data.text.black.re,(col*128,row*128))
             elif data.board[row][col] == "RO":
-                data.fenetre.blit(data.text.black.ro,(col*130 - 35,row*130-70))
+                data.fenetre.blit(data.text.black.ro,(col*128,row*128))
             elif data.board[row][col] == "FO":
-                data.fenetre.blit(data.text.black.fo,(col*130,row*130))
+                data.fenetre.blit(data.text.black.fo,(col*128,row*128))
             elif data.board[row][col] == "CA":
-                data.fenetre.blit(data.text.black.ca,(col*130,row*130))
+                data.fenetre.blit(data.text.black.ca,(col*128,row*128))
             elif data.board[row][col] == "TO":
-                data.fenetre.blit(data.text.black.to,(col*130,row*130))
+                data.fenetre.blit(data.text.black.to,(col*128,row*128))
             elif data.board[row][col] == "PI":
-                data.fenetre.blit(data.text.black.pi,(col*130,row*130))
+                data.fenetre.blit(data.text.black.pi,(col*128,row*128))
 
 
 def init_text():
     text = texture()
-    text.whitepace = pygame.image.load(resource_path("text/pace/whitepace.png")).convert()
+    text.board = pygame.image.load(resource_path("text/pace/board.png")).convert_alpha()
+    text.board = pygame.transform.scale(text.board, (1024, 1024))
     text.greenpace = pygame.image.load(resource_path("text/pace/greenpace.png")).convert()
+    text.greenpace = pygame.transform.scale(text.greenpace, (128, 128))
     text.redpace = pygame.image.load(resource_path("text/pace/redpace.png")).convert()
+    text.redpace = pygame.transform.scale(text.redpace, (128, 128))
     text.orangepace = pygame.image.load(resource_path("text/pace/orangepace.png")).convert()
+    text.orangepace = pygame.transform.scale(text.orangepace, (128, 128))
     text.white.re = pygame.image.load(resource_path("text/white/re.png")).convert_alpha()
+    text.white.re = pygame.transform.scale(text.white.re, (128, 128))
     text.white.ro = pygame.image.load(resource_path("text/white/ro.png")).convert_alpha()
+    text.white.ro = pygame.transform.scale(text.white.ro, (128, 128))
     text.white.to = pygame.image.load(resource_path("text/white/to.png")).convert_alpha()
+    text.white.to = pygame.transform.scale(text.white.to, (128, 128))
     text.white.fo = pygame.image.load(resource_path("text/white/fo.png")).convert_alpha()
+    text.white.fo = pygame.transform.scale(text.white.fo, (128, 128))
     text.white.ca = pygame.image.load(resource_path("text/white/ca.png")).convert_alpha()
+    text.white.ca = pygame.transform.scale(text.white.ca, (128, 128))
     text.white.pi = pygame.image.load(resource_path("text/white/pi.png")).convert_alpha()
+    text.white.pi = pygame.transform.scale(text.white.pi, (128, 128))
     text.black.re = pygame.image.load(resource_path("text/black/re.png")).convert_alpha()
+    text.black.re = pygame.transform.scale(text.black.re, (128, 128))
     text.black.ro = pygame.image.load(resource_path("text/black/ro.png")).convert_alpha()
+    text.black.ro = pygame.transform.scale(text.black.ro, (128, 128))
     text.black.to = pygame.image.load(resource_path("text/black/to.png")).convert_alpha()
+    text.black.to = pygame.transform.scale(text.black.to, (128, 128))
     text.black.fo = pygame.image.load(resource_path("text/black/fo.png")).convert_alpha()
+    text.black.fo = pygame.transform.scale(text.black.fo, (128, 128))
     text.black.ca = pygame.image.load(resource_path("text/black/ca.png")).convert_alpha()
+    text.black.ca = pygame.transform.scale(text.black.ca, (128, 128))
     text.black.pi = pygame.image.load(resource_path("text/black/pi.png")).convert_alpha()
+    text.black.pi = pygame.transform.scale(text.black.pi, (128, 128))
     return text
 
 def init():
     data = info()
-    data.fenetre = pygame.display.set_mode((1025, 1025))
+    data.fenetre = pygame.display.set_mode((1024, 1024))
     data.board = [
         ["to","ca","fo","re","ro","fo","ca","to"],
         ["pi","pi","pi","pi","pi","pi","pi","pi"],
@@ -438,10 +440,10 @@ def main():
                         t = "."
                         temp[0] = -1
                         temp[1] = -1
-                        # if p == 0:
-                        #     p = 1
-                        # else:
-                        #     p = 0
+                        if p == 0:
+                            p = 1
+                        else:
+                            p = 0
                         clear_board(data)
                 elif event.key == K_LEFT and data.xp != 0:
                     data.xp = data.xp - 1
